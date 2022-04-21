2e50ecf8fbd1a03ee64749a4fabb6b8c0aac95a2
draganaZdravkovic <dragana.zdravkovic602@gmail.com>
2022-04-22 01:03:58 +0200

    new lesson activity interface

--- app/src/main/java/com/example/naucime/ProfesorDashboardActivity.kt
+++ app/src/main/java/com/example/naucime/ProfesorDashboardActivity.kt
@@ -1,17 +1,22 @@
 package com.example.naucime
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class ProfesorDashboardActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profesor_dashboard)
 
-        val btAddLesson: Button = findViewById(R.id.btAddLesson)
-        btAddLesson.setOnClickListener {
+        val btNewLesson: Button = findViewById(R.id.btNewLesson)
+        btNewLesson.setOnClickListener {
 
+            val intent = Intent(this, AddLessonActivity::class.java)
+            startActivity(intent)
         }
     }
+
 }
