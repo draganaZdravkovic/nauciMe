c6c32afc81a2112f63971eb2f28483079608f51e
mkordic <mirko22kordic@gmail.com>
2022-04-21 23:40:34 +0200

    testCommit

--- app/src/main/java/com/example/naucime/ProfesorDashboardActivity.kt
+++ app/src/main/java/com/example/naucime/ProfesorDashboardActivity.kt
@@ -2,10 +2,16 @@ package com.example.naucime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ProfesorDashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profesor_dashboard)
+
+        val btAddLesson: Button = findViewById(R.id.btAddLesson)
+        btAddLesson.setOnClickListener {
+
+        }
     }
 }
