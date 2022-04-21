1ddf474ce57242bbf85a62f7618155e37da7343b
draganaZdravkovic <dragana.zdravkovic602@gmail.com>
2022-04-22 01:07:23 +0200

    new lesson activity interface

---
+++ app/src/main/java/com/example/naucime/AddLessonActivity.kt
@@ -0,0 +1,11 @@
+package com.example.naucime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddLessonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_lesson)
+    }
+}
